19481a1cf26f407a9de11534a1967cea1769a6a8
dzuvan <hrnjak.jovan1@gmail.com>
2018-01-20 21:53:35 +0100

    Added file creation function.

--- code/BigCinestar.h
+++ code/BigCinestar.h
@@ -1,34 +1,37 @@
-
-/* 
- * File:   BigCinestar.h
- * Author: dzuvan
- *
- * Created on 13.01.2018., 20.15
- */
+#ifndef BIGCINESTAR_H
+#define BIGCINESTAR_H
 
 #include <stdio.h>
 #include "definitions.h"
 
-#ifndef BIGCINESTAR_H
-#define BIGCINESTAR_H
-
 typedef enum tip_projekcije {
-    TWO,
-    THREE,
-    FOUR
+    DVA,
+    TRI,
+    CETIRI
 } tip_projekcije;
 
 struct film {
-    int evidencioni_broj[EVIDENCIJA];
-    char naziv[MAX_NAZIV];
-    int prosecna_ocena;
     tip_projekcije tip;
-    int duzina_trajnja;
+    int prosecna_ocena;
+    char evidencioni_broj[EVIDENCIJA];
+    char naziv[MAX_NAZIV];
+    int duzina_trajanja;
     char vreme_i_datum[12]; //dd mm yyyy hh mm
+    bool status;
+    struct film *sledeci;
 };
 
 struct baket {
-    film film_baket[FAKTOR];
+    film film_baket[FAKTOR_BAKETIRANJA];
 };
+
+struct trazi {
+    int pozicija;
+    int broj;
+    bool status;
+};
+
+int transformacija_kljuca(int kljuc);
+void napravi_datoteku(char* temp);
 
 #endif /* BIGCINESTAR_H */
--- code/main.cpp
+++ code/main.cpp
@@ -1,58 +1,48 @@
-
-/* 
- * File:   main.cpp
- * Author: dzuvan
- *
- * Created on 13.01.2018., 16.21
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "BigCinestar.h"
 #include "definitions.h"
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+FILE* datoteka;
+FILE* pomocna_datoteka;
 
-    FILE *f;
-    char n[40];
-    char c;
-    int broj_redova;
-    int broj;
-
-    printf("Unesite naziv datoteke(do 40 karaktera): \n");
-    scanf("%s", n);
-
-
-    printf("Unesite broj redova:\n");
-    scanf("%d", &broj_redova);
-
-    for (unsigned int i = 0; i < broj_redova; i++) {
-        printf("Unesite broj");
-        fscanf(f, "%d", &broj);
-    }
-
-    if ((f = fopen(n, "w")) == NULL) {
-        printf("Datoteka <%s> nije uspesno otvorena.\n", n);
-        exit(1);
-    }
-
-    printf("Datoteka<%s> je uspesno otvorena.\n", n);
-    printf("Tekstualni sadrzaj datoteke. <%s>...\n", n);
-
-    //        while ((c = fgetc(f)) != EOF) {
-    //            putchar(c);
-    //        }
-    //        
-
-    if (fclose(f) == EOF) {
-        printf("\tNastupila je greska tokom zatvaranja");
-    } else {
-        printf("\tZatvarnje uspesno!");
-    }
-
+int main() {
     return 0;
+}
+
+
+void napravi_datoteku(char* temp) {
+    struct baket baket;
+    struct film film;
+    char aktivna_datoteka[20];
+    char trenutna[20];
+    int zapis;
+
+    if ((datoteka = fopen(temp, "wb+")) == NULL) {
+        printf("\nDatoteka %s nije kreirana!", temp);
+        fclose(datoteka);
+        return;
+    }
+
+    for (unsigned int i = 0; i < FAKTOR_BAKETIRANJA; i++) {
+        baket.film_baket[i].tip = DVA;
+        baket.film_baket[i].prosecna_ocena = 0;
+        baket.film_baket[i].evidencioni_broj[0] = '\0';
+        baket.film_baket[i].naziv[0] = '\0';
+        baket.film_baket[i].duzina_trajanja = 0;
+        baket.film_baket[i].vreme_i_datum[0] = '\0';
+        baket.film_baket[i].status = false;
+    }
+
+    for (unsigned int j = 0; j < BROJ_BAKETA; j++) {
+      if (fwrite(&baket, sizeof(struct baket), 1, datoteka) != 1) {
+          puts("Greska prilikom upisa u datoteku!\n");
+      }
+    }
+
+    printf("\nDatoteka %s uspesno kreirana!", temp);
+    strcpy(aktivna_datoteka, temp);
+
 }
